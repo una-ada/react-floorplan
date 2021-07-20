8524e3a1ce4c79e3dfe8e4631c97a2aa0d21b30a
Una <trewbot@gmail.com>
2021-07-20 12:36:16 -0700

    feat(comp): add Bedroom component

---
+++ src/Bedroom.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Bedroom = props => <div className="Bedroom">Bedroom {props.bedNum}</div>;
+
+export default Bedroom;
--- src/FloorPlan.js
+++ src/FloorPlan.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Bedroom from './Bedroom';
 import Kitchen from './Kitchen';
 import LivingRoom from './LivingRoom';
 
 const FloorPlan = props => (
   <div className="FloorPlan">
+    <Bedroom bedNum={1} />
     <Kitchen />
     <LivingRoom />
+    <Bedroom bedNum={2} />
+    <Bedroom bedNum={3} />
   </div>
 );
 
