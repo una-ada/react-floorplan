60c31e8d3d2b0fb3ecb13fe65f7a778fe15d4f94
Una <trewbot@gmail.com>
2021-07-20 12:32:18 -0700

    feat(comp): add LivingRoom

--- src/FloorPlan.js
+++ src/FloorPlan.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react';
+import Kitchen from './Kitchen';
+import LivingRoom from './LivingRoom';
 
-const FloorPlan = (props) => <div className="FloorPlan"></div>;
+const FloorPlan = props => (
+  <div className="FloorPlan">
+    <Kitchen />
+    <LivingRoom />
+  </div>
+);
 
 export default FloorPlan;
