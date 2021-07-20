8574dbd7fcf451013d33e2898b63c33d5670ab25
Una <trewbot@gmail.com>
2021-07-20 12:42:36 -0700

    feat(comp): add Oven

--- src/Kitchen.js
+++ src/Kitchen.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React from 'react';
+import Oven from './Oven';
 
-const Kitchen = (props) => <div className="Kitchen"></div>;
+const Kitchen = props => (
+  <div className="Kitchen">
+    <Oven />
+  </div>
+);
 
 export default Kitchen;
---
+++ src/Oven.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Oven = props => <div className="Oven">Oven</div>;
+
+export default Oven;
