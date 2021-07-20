907ddd2c870d40dfd16fe75a6b29724f03dc3e7f
Una <trewbot@gmail.com>
2021-07-20 12:43:31 -0700

    feat(comp): add Sink

--- src/Kitchen.js
+++ src/Kitchen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Oven from './Oven';
+import Sink from './Sink';
 
 const Kitchen = props => (
   <div className="Kitchen">
     <Oven />
+    <Sink />
   </div>
 );
 
---
+++ src/Sink.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Sink = props => <div className="Sink">Sink</div>;
+
+export default Sink;
